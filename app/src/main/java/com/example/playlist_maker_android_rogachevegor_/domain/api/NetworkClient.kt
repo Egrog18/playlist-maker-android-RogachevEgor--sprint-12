c7330882c14184e66f983e5f80696122d527c278
Egor Rogachev <Yegor18yegor@yandex.ru>
2026-06-01 00:37:57 +0600

    Empty review

--- app/src/main/java/com/example/playlist_maker_android_rogachevegor_/domain/api/NetworkClient.kt
+++
@@ -1,7 +0,0 @@
-package com.example.playlist_maker_android_rogachevegor_.domain.api
-
-import com.example.playlist_maker_android_rogachevegor_.data.dto.BaseResponse
-
-interface NetworkClient {
-    fun doRequest(dto: Any): BaseResponse
-}
--- app/src/main/java/com/example/playlist_maker_android_rogachevegor_/domain/api/SearchHistoryRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.playlist_maker_android_rogachevegor_.domain.api
-
-interface SearchHistoryRepository {
-    fun addEntry(word: String)
-
-    suspend fun getEntries(): List<String>
-}
